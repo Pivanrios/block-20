2aac78af0cfc8f08ca89de8dfaf54e88f0919801
Pedro Perez <pipr96@gmail.com>
2023-05-15 21:41:31 -0700

    Print title and ul of names

--- index.js
+++ index.js
@@ -10,6 +10,36 @@ const users = [
 ];
 
 function main() {
+    console.log("We're in!")
+    //access the main div 
+    const root = document.querySelector("#root")
+    console.log("element root created:",root)
+
+    //Add the title h1 on the page
+    const title = document.createElement("h1")//create element
+    title.innerText = "FREELANCERS"//add title
+    root.appendChild(title)//append new node to 
+    console.log("Title:",title.innerText)
+
+    //create an unorder list element
+    const unorderList = document.createElement("ul")
+    unorderList.innerText = "Users:" //channge innerText
+    console.log("ul:", unorderList.innerText)
+
+    //Loop thru our users and get the name property to render it on the page.
+    users.forEach((element) =>{
+        console.log("name:", element.name)
+        //create list items and add them to the ul and render it.
+        const li = document.createElement("li");// create an element list
+        li.innerText = element.name; //change the innertext to name value
+        unorderList.appendChild(li)//add list item to the ul.
+    })
+    console.log("Out of the loop.")
+
+    //append list to root element
+    root.appendChild(unorderList)
+    console.log("html code", root.innerHTML)
+
 
 }
 
